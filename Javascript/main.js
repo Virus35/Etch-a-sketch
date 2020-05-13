985688018f0ce4f00b30b9a605e8b8e8845e55fc
Virus35 <mehulsaxena92@gmail.com>
2020-05-13 18:32:18 +0530

    Added clearGrid, RandomFill and colorPicker for user

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,9 +1,9 @@
 function createGrid(n){
     for(let i = 0; i < n*n; i++){
-        cont.setAttribute('style',`display:grid; width: 700px; height: 700px; grid-template-columns: repeat(${n}, 1fr); grid-auto-rows: auto ; grid-gap: 1px`);
+        cont.setAttribute('style',`display:grid; width: 650px; height: 650px; grid-template-columns: repeat(${n}, 1fr); grid-auto-rows: auto ; grid-gap: 1px`);
         const newDiv = document.createElement('div');
         newDiv.classList.add('block');
-        newDiv.addEventListener('mouseenter', (e) => e.target.style.backgroundColor = 'red');
+        newDiv.addEventListener('mouseenter', (e) => e.target.style.backgroundColor = color);
         cont.appendChild(newDiv);
     }
 }
@@ -13,11 +13,30 @@ function removeGrid(){
         cont.removeChild(ele);
     });
 }
+function clearGrid(){
+    const divElem = document.querySelectorAll('#container > .block');
+    divElem.forEach((ele) => {
+        ele.style.backgroundColor = '#FFF';
+    });
+}
+function randomRGBValue(){
+    return Math.floor(Math.random()*256);
+}
+function randomFill(){
+    const divElem = document.querySelectorAll('#container > .block');
+    divElem.forEach((ele) => {
+        ele.style.backgroundColor = `rgb(${randomRGBValue()}, ${randomRGBValue()}, ${randomRGBValue()})`;
+    });
+}
 
 const cont = document.getElementById('container');
-const btn = document.getElementById('createNewGrid');
+const newGridBtn = document.getElementById('createNewGrid');
+const clearGridBtn = document.getElementById('clearGrid');
+const randomBtn = document.getElementById('randomBtn');
+const fillColorValue = document.getElementById('colorPicker');
+let color = '#555555';
 createGrid(16);
-btn.addEventListener('click', function(){
+newGridBtn.addEventListener('click', function(){
     removeGrid();
     let size = prompt('Enter the grid edge length(1-64)');
     if(size >= 1 && size <= 64){
@@ -28,5 +47,8 @@ btn.addEventListener('click', function(){
         createGrid(16);
     }
 });
-// const elems = document.querySelectorAll('#container > .block');
-// elems.forEach((elem) => elem.addEventListener('mouseover', (e) => e.target.style.backgroundColor = 'red'));
+clearGridBtn.addEventListener('click', clearGrid);
+randomBtn.addEventListener('click', randomFill);
+fillColorValue.addEventListener('change', function(){
+    color = fillColorValue.value;
+});
